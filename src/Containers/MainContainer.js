304da17312c75e3468a85eed709119f87f33b911
github username <github email address>
2019-10-05 12:54:37 -0400

    adds top palm image

--- src/Containers/MainContainer.js
+++ src/Containers/MainContainer.js
@@ -6,13 +6,14 @@ import EventCal from '../Components/EventCal'
 import Store from '../Components/Store'
 import Footer from '../Components/Footer'
 import logo from '../logo.svg'
+import palm from '../palm-top.svg'
 
 export default class MainContainer extends Component {
   render() {
     return(
       <Fragment>
+        <img className='palm-top' src={ palm } alt='palm leaf'/>
         <img className='logo-main' src={ logo } alt='logo'/>
-
         <div className='main'>
           <h4>616 HALSEY ST BROOKLYN, NY 11233</h4>
           <h4></h4>
