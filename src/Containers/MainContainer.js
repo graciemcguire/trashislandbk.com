0fdd01a9e0aa98511b412279e02e907c6ebe626c
github username <github email address>
2019-10-05 15:07:42 -0400

    fix h4

--- src/Containers/MainContainer.js
+++ src/Containers/MainContainer.js
@@ -11,16 +11,18 @@ export default class MainContainer extends Component {
         <img className='palm-top' src={ palm } alt='palm leaf'/>
         <img className='logo-main' src={ logo } alt='logo'/>
         <div className='main'>
-          <h4>616 HALSEY ST BROOKLYN, NY 11233</h4>
+          <h4>616 HALSEY ST</h4>
+          <h4>BROOKLYN, NY 11233</h4>
           <h4></h4>
           <h4>OPEN 8-8 DAILY</h4>
 
         </div>
-        <div className='links'>
-          <img className='link-images' src={ insta } alt='instagram'/>
-          <img className='link-images' src={ email } alt='email'/>
-
-        </div>
+          <a href="http://www.instagram.com/trashislandbk" target="_blank">
+            <img className='link-images' src={ insta } alt='instagram'/>
+          </a>
+          <a href="mailto:trashislandbk@gmail.com">
+            <img className='link-images' src={ email } alt='email'/>
+          </a>
       </ Fragment>
     )
   }
