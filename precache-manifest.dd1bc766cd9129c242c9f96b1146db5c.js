f583c162570eb59059fce6a4ea5b931cd6beab0b
github username <github email address>
2019-10-06 14:11:46 -0400

    Updates

---
+++ precache-manifest.dd1bc766cd9129c242c9f96b1146db5c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f17987c07debb20f89d5aa20a06f51b",
+    "url": "trashislandbk.com/index.html"
+  },
+  {
+    "revision": "1eaa4293b3724394dac8",
+    "url": "trashislandbk.com/static/css/main.f498d01b.chunk.css"
+  },
+  {
+    "revision": "32ba428a442bce32e14e",
+    "url": "trashislandbk.com/static/js/2.249420ef.chunk.js"
+  },
+  {
+    "revision": "1eaa4293b3724394dac8",
+    "url": "trashislandbk.com/static/js/main.62caa65a.chunk.js"
+  },
+  {
+    "revision": "879e307c4b97fca1a959",
+    "url": "trashislandbk.com/static/js/runtime~main.c0a545f0.js"
+  },
+  {
+    "revision": "cd1d4bce9b8040b2bcb80f3fdbea27ed",
+    "url": "trashislandbk.com/static/media/email.cd1d4bce.svg"
+  },
+  {
+    "revision": "068d3f092bc991cdce347455c0b9cd25",
+    "url": "trashislandbk.com/static/media/insta.068d3f09.svg"
+  },
+  {
+    "revision": "27a03e5c9c074c55883847efdcd0f67f",
+    "url": "trashislandbk.com/static/media/logo.27a03e5c.svg"
+  },
+  {
+    "revision": "afece8b72c63e2d78dac3f8aed277c7f",
+    "url": "trashislandbk.com/static/media/palm-top.afece8b7.svg"
+  },
+  {
+    "revision": "b2d1d93d47ebc247274de53b8d78dd18",
+    "url": "trashislandbk.com/static/media/trashisland_menu.b2d1d93d.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/trash-island/precache-manifest.a0dacde75ca0549100e2c3cf88614687.js"
+  "trashislandbk.com/precache-manifest.dd1bc766cd9129c242c9f96b1146db5c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/trash-island/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("trashislandbk.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
